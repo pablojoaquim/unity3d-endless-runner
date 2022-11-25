a1ea21a54643b0f346fe2ef4e19a7532ee8806b4
Pablo Joaquim <pablojoaquim@gmail.com>
2022-11-25 19:30:04 -0300

    The player is running throught the endless road!

---
+++ Assets/_Import/Scripts/RoadSpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class RoadSpawner : MonoBehaviour
+{
+    public List<GameObject> roads;
+    private float offset = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (roads != null && roads.Count > 0)
+        {
+            roads = roads.OrderBy(r => r.transform.position.z).ToList();
+        }
+    }
+
+    public void MoveRoad()
+    {
+        GameObject moveRoad = roads[0];
+
+        roads.Remove(moveRoad);
+        float newZ = roads[roads.Count - 1].transform.position.z + offset;
+        moveRoad.transform.position = new Vector3(0,0,newZ);
+        roads.Add(moveRoad);
+    }
+}
