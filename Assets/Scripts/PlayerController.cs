17f4da6902c842bf8f49fa726f0bfdd1ad25ca58
Pablo Joaquim <pablojoaquim@gmail.com>
2022-11-27 01:29:00 -0300

    The movement of the car is now smooth, and the position calculation is done according to the controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,14 +12,17 @@ public class PlayerController : MonoBehaviour
     {
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        float hMovement = Input.GetAxis("Horizontal") * movementSpeed / 2;
-        float vMovement = Input.GetAxis("Vertical") * movementSpeed;
+        float hMovement = Input.GetAxis("Horizontal") * movementSpeed / 2 * Time.deltaTime;
+        float vMovement = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
 
-        transform
-            .Translate(new Vector3(vMovement, 0, -1*hMovement) * Time.deltaTime);
+        // transform
+        //     .Translate(new Vector3(vMovement, 0, -1*hMovement) * Time.deltaTime);
+        transform.position =
+            new Vector3(transform.position.x + hMovement,
+                transform.position.y,
+                transform.position.z + vMovement);
     }
 
     private void OnTriggerEnter (Collider collider)
